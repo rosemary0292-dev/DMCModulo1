f12c586e939bb3206491f078f6fdfb11c2bd047d
rosemary0292-dev <rosemary0292@gmail.com>
2026-08-05 21:05:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,6 @@ import streamlit as st
 st.title("Especializacion  Python for Analytics")
 st.sidebar.title("Parametros")
 st.write("Elaborado por: Carmela Contreras")
+valor_inicial=st.number_input("Ingrese el valor inicial")
+valor_final=st.number_input("Ingrese el valor final")
+lista_numeros=list(range(valor_inicial,valor_final))
