8f0dd5e45f9231d75b77ffdeb0cdf88d5ae29a2b
rosemary0292-dev <rosemary0292@gmail.com>
2026-08-05 22:03:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,15 @@ import streamlit as st
 st.title("Especializacion  Python for Analytics")
 st.sidebar.title("Parametros")
 st.write("Elaborado por: Carmela Contreras")
-valor_inicial=st.number_input("Ingrese el valor inicial")
-valor_final=st.number_input("Ingrese el valor final")
-lista_numeros=list(range(int(valor_inicial),int(valor_final)))
-st.write(lista_numeros)
+
+modulos=st.sidebar.selectbox("Seleccione un Modulo",["Modulo Listas","Modulo Arreglos","Modulo Funciones"])
+if modulos=="Modulo Listas":
+  st.write("Bienvenido al modulo Lista")
+  valor_inicial=st.number_input("Ingrese el valor inicial")
+  valor_final=st.number_input("Ingrese el valor final")
+  lista_numeros=list(range(int(valor_inicial),int(valor_final)))
+  st.write(lista_numeros)
+elif modulos=="Modulo Arreglos":
+  st.write("Bienvenido al modulo Arreglos")
+else:
+  st.write("Bienvenido al modulo Funciones")
