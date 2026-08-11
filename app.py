923a9d37dbae3c20b5830d21e27cd8a42241b0a3
rosemary0292-dev <rosemary0292@gmail.com>
2026-08-10 22:12:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import numpy as np
 
 st.title("Especializacion  Python for Analytics")
 st.sidebar.title("Parametros")
@@ -17,5 +17,9 @@ if modulos=="Modulo Listas":
   st.write(lista_numeros)
 elif modulos=="Modulo Arreglos":
   st.write("Bienvenido al modulo Arreglos")
+  cantidad_elementos=st.slider("Seleccione la Cantidad de elementos de su arreglo",1,100)
+  cantidad_arreglo=np.arange(cantidad_elementos)
+  st.write(cantidad_arreglo)
+
 else:
   st.write("Bienvenido al modulo Funciones")
