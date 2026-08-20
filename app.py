bb0037630c4d337f33a86ea39436c39db1e2fd11
rosemary0292-dev <rosemary0292@gmail.com>
2026-08-19 22:25:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import numpy as np
 import libreria_funciones as lf
+import pandas as pd
+
 st.title("Especializacion  Python for Analytics")
 st.sidebar.title("Parametros")
 st.write("Elaborado por: Carmela Contreras")
@@ -20,7 +22,18 @@ elif modulos=="Modulo Arreglos":
   cantidad_elementos=st.slider("Seleccione la Cantidad de elementos de su arreglo",1,100)
   cantidad_arreglo=np.arange(cantidad_elementos)
   st.write(cantidad_arreglo)
-
+elif modulo=="Archivos"
+  archivo = st.sidebar.file_uploader("Seleccione su archivo")
+  if archivo is not None:
+    st.write("Su archivo ha sido Cargado")
+    if archivo.name.endwith(".csv"):
+      datos=pd.read_csv(archivo)
+    elif archivo.name.endwith(".xlsx"):
+      datos=pd.read_excel(archivo)
+    else:
+      st.write("No es un archivo compatible")
+  else:
+     st.write("Cargue su archivo")
 else:
   st.write("Bienvenido al módulo de Funciones")
   capital_inicial = st.number_input("Capital inicial", min_value=0.0, value=1000.0)
